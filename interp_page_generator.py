e5330b2fe461fa564c7264f1072ea3e3f1ebc97a
Clinton <c.s.cao@tudelft.nl>
2023-10-23 17:44:08 +0200

    Updated examples with visualization and removed unused svg files.

--- interp_page_generator.py
+++ interp_page_generator.py
@@ -18,11 +18,15 @@ def get_interpretation_pages(project_interpretation_folder_path: str) -> list:
 
 
 def generate_overview_page_for_project(output_folder_path, project_name: str, project_interpretation_folder_path: str, project_interpretation_pages: list):
-    doc = dominate.document(title=f'Overview of interpretations generated for {project_name}')
+    doc = dominate.document(title=f'Visualization of Non-Conformances and Overview of interpretations generated for {project_name}')
     with doc.head:
         link(rel='stylesheet', href='css/overview_page_stylesheet.css')
     
     with doc:
+        h1(f'Non-conformance visualization for {project_name}')
+        with div(id='ncf_visualization'):
+            visualize_ncf_path = project_interpretation_folder_path.split('interpretations')[0] + 'visualization/'
+            img(src=f'{visualize_ncf_path}plantuml.png')
         h1(f'Overview of interpretations generated for {project_name}')
         with div(id='interpretations_list'):
             for page in project_interpretation_pages:
@@ -37,7 +41,6 @@ def generate_overview_page_for_project(output_folder_path, project_name: str, pr
                 h3(f'{capitalized_type} non-conformance between {involved_services}:')
                 a('Click here to view the interpretation for the ' + ncf_type + ' non-conformance between ' + involved_services , href=f'{project_interpretation_folder_path}/{page}')
 
-    
     with open(f'{output_folder_path}/{project_name}_overview.html', 'w') as f:
         f.write(doc.render())
     
@@ -49,7 +52,7 @@ def main():
     allow us to get a directory view of subfolders, we would have to manually create this view ourselves.
     """
     examples_folder_path = 'interpretation_examples'
-    projects = ['ewolff_microservice', 'shabbirdwd53_springboot_microservice', 'spring-petclinic_spring-petclinic-microservices', 'sqshq_piggymetrics']
+    projects = ['ewolff_microservice', 'shabbirdwd53_springboot-microservice', 'spring-petclinic_spring-petclinic-microservices', 'sqshq_piggymetrics']
     project_to_interp_pages_mapping = dict()
     for project in projects:
         project_to_interp_pages_mapping[project] = get_interpretation_pages(f'{examples_folder_path}/{project}/interpretations/')
